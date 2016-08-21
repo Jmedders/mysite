4080a3f6d3ad7e11707bb00473eac616cd5ec9e2
Jeff <medders.jeffrey@gmail.com>
2016-08-20 20:35:42 -0600

    more work on controllers, ngshow, ngclicks

--- public/javascripts/controllers/controllers.js
+++ public/javascripts/controllers/controllers.js
@@ -6,20 +6,4 @@ $scope.view.education = false;
 $scope.view.experience = false;
 $scope.view.languages = false;
 $scope.view.aboutme = false;
-$scope.view.portfoliofn = function(){
-  $scope.view.portfolio = true;
-};
-$scope.view.educationfn = function(){
-  $scope.view.education = true;
-};
-$scope.view.experiencefn = function(){
-  $scope.view.experience = true;
-};
-$scope.view.languagesfn = function(){
-  $scope.view.languages = true;
-};
-$scope.view.aboutmefn = function(){
-  $scope.view.aboutme = true;
-};
-
 }])
