c9ddc848d033f7860c77dc1cd8bf753732508d9c
Jeff <medders.jeffrey@gmail.com>
2016-08-20 20:14:03 -0600

    doing work with controllers

--- public/javascripts/controllers/controllers.js
+++ public/javascripts/controllers/controllers.js
@@ -1,3 +1,25 @@
 app.controller('mainController', ['$scope', '$http', function($scope, $http){
-  $scope.myself = "Jeff"
+
+$scope.view = {};
+$scope.view.portfolio = false;
+$scope.view.education = false;
+$scope.view.experience = false;
+$scope.view.languages = false;
+$scope.view.aboutme = false;
+$scope.view.portfoliofn = function(){
+  $scope.view.portfolio = true;
+};
+$scope.view.educationfn = function(){
+  $scope.view.education = true;
+};
+$scope.view.experiencefn = function(){
+  $scope.view.experience = true;
+};
+$scope.view.languagesfn = function(){
+  $scope.view.languages = true;
+};
+$scope.view.aboutmefn = function(){
+  $scope.view.aboutme = true;
+};
+
 }])
